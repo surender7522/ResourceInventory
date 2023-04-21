9cbbd90ce207a3f4d16221c173cce840b6c3567c
Surender Singh Lamba <sslamba10@gmail.com>
2023-04-21 23:01:01 +0900

    Added basics of server in fastapi

---
+++ main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
+from fastapi.staticfiles import StaticFiles
+from starlette_exporter import PrometheusMiddleware, handle_metrics
+from controllers.model_controller import model_router
+from controllers.resource_controller import resource_router
+import logger
+app = FastAPI(title="Resource Inventory",docs_url=None, redoc_url=None)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.include_router(model_router,tags=["Model Apis"])
+app.include_router(resource_router,tags=["Resource Apis"])
+
+app.add_middleware(PrometheusMiddleware)
+app.add_route("/metrics", handle_metrics)
+logger.initialize()
+logger.logger.info("Starting app...")
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="/static/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui.css",
+    )
+
+
+@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
+
+
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - ReDoc",
+        redoc_js_url="/static/redoc.standalone.js",
+    )
